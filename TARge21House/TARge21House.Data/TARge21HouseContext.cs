404396d2de298530aac9dc514c89ce0a07b40f20
klill1 <kert.lillemets@gmail.com>
2023-03-24 22:15:16 +0200

    Preparation for migration, DbContext etc

---
+++ TARge21House/TARge21House.Data/TARge21HouseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TARge21House.Core.Domain;
+
+namespace TARge21House.Data
+{
+    public class TARge21HouseContext : DbContext
+    {
+        public TARge21HouseContext(DbContextOptions<TARge21HouseContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<House> Houses { get; set; }
+    }
+}
--- TARge21House/TARge21House/Program.cs
+++ TARge21House/TARge21House/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using TARge21House.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<TARge21HouseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
