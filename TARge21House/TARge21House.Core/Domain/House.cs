c835e27e1dc43ab523e9cebe8ede291d9cb4a348
klill1 <kert.lillemets@gmail.com>
2023-03-24 22:03:34 +0200

    HouseDto and Domain

---
+++ TARge21House/TARge21House.Core/Domain/House.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TARge21House.Core.Domain
+{
+    public class House
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public int Size { get; set; }
+        public int RoomCount { get; set; }
+        public int Floors { get; set; }
+        public string Color { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
---
+++ TARge21House/TARge21House.Core/Dto/HouseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TARge21House.Core.Dto
+{
+    public class HouseDto
+    {
+        public Guid Id { get; set; }
+        public int Size { get; set; }
+        public int RoomCount { get; set; }
+        public int Floors { get; set; }
+        public string Color { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
